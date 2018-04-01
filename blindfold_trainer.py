c1721593871b9136f92bbecee9b1c7c2bbfdf5e2
Andy Liu <andy@liuonline.com>
2018-04-01 10:12:14 -0400

    Add brother square drill to trainer

--- blindfold_trainer.py
+++ blindfold_trainer.py
@@ -37,7 +37,36 @@ def colors(num):
     click.echo(exit_msg)
     click.echo(time_taken)
 
+@click.command()
+@click.argument('num', default=1)
+def brothers(num):
+    click.echo("Running drill with " + str(num) + " trials...")
+
+    start_time = time.time()
+    score = 0
+    for x in range(num):
+        square = Square.random()
+        click.echo('\n')
+
+        # while(input_square not in white_names.union(black_names)):
+            # input_square = input("That's not a square, mate! Try again\n")
+
+        brother_square = square.brother_square().name
+        if (input_name == brother_square):
+            click.echo("Correct!")
+            score = score + 1
+        else:
+            response = "Incorrect, " + square.name + "'s brother' is " + brother_square
+            click.echo(response)
+
+    end_time = time.time()
+    exit_msg = "\nNice job mate, you scored " + str(score) + '/' + str(num)
+    time_taken = str(end_time - start_time) + "s"
+    click.echo(exit_msg)
+    click.echo(time_taken)
+
 cli.add_command(colors)
+cli.add_command(brothers)
 
 if __name__ == "__main__":
     cli()
