d3e1e08136647b2951d7150d2555e57f24a3f8af
Andy Liu <andy@liuonline.com>
2018-05-22 21:39:34 -0400

    Implement command line knight path test

--- blindfold_trainer.py
+++ blindfold_trainer.py
@@ -2,8 +2,10 @@ import click
 import time
 from functools import wraps
 from chess_square import (Square,
+                          coord_to_name,
                           is_valid_square_name)
-
+from chess_piece import (knight_moves,
+                         find_shortest_path)
 
 def reporter(func):
     @wraps(func)
@@ -56,6 +58,9 @@ def colors(num):
 @click.argument('num', default=1)
 @reporter
 def brothers(num):
+    """
+    Reflects the square across the a1-h8 diagonal. The brother square is in the same relative location if you rotated the board 180 degrees. 
+    """
     score = 0
     for x in range(num):
         square = Square.random()
@@ -75,8 +80,37 @@ def brothers(num):
     return score
 
 
+@click.command()
+@click.argument('num', default=1)
+@reporter
+def knight_path(num):
+    """
+    Finds the shortest path between two squares, one on the first rank and one on the 
+    eighth rank. 
+    """
+    score = 0
+    for x in range(num):
+        starting_square = Square.random_inside((7,0), (7,7))
+        target_square = Square.random_inside((0,0), (0,7))
+        click.echo('\n')
+        shortest_path = find_shortest_path(starting_square.coord, target_square.coord, knight_moves)
+        
+        input_len = input(starting_square.name + " to " + target_square.name + '\n')
+
+        if (int(input_len) == shortest_path[0]):
+            click.echo("Correct!")
+            score = score + 1
+        else:
+            response = "Incorrect, " + target_square.name + " can be reached in " + str(shortest_path[0]) + " moves, e.g." + '\n'
+            response += ' '.join(list(map(coord_to_name, shortest_path[1])))
+            click.echo(response)
+
+    return score
+
+
 cli.add_command(colors)
 cli.add_command(brothers)
+cli.add_command(knight_path)
 
 
 if __name__ == "__main__":
