767f5a0e0ed0f3c104a7407daeb13cffec660175
Andy Liu <andy@liuonline.com>
2018-05-22 20:53:10 -0400

    Implement BFS shortest path algorithm

--- chess_piece.py
+++ chess_piece.py
@@ -1,10 +1,7 @@
 import string
 import functools
-from chess_square import (Square,
-                          name_to_coord,
-                          coord_to_name,
-                          is_valid_square_coord,
-                          is_valid_square_name)
+from collections import deque
+from chess_square import is_valid_square_coord
 
 
 # Note: if coords are numpy vectors then we wouldn't need this function
@@ -14,13 +11,12 @@ def coord_add(coordA, coordB):
     return (x1 + x2, y1 + y2)
 
 
-def knight_moves(origin_square):
+def knight_moves(origin):
     """
-    Takes an origin square and applies all possible knight-move vectors
-    Square origin_square: the origin square
-    returns filter-iterable: all legal squares reachable by a knight
+    Takes an origin coordinate and applies all possible knight-move vectors
+    int tuple origin: the origin coordinate
+    returns filter-iterable: all legal coordinates reachable by a knight
     """
-    origin = origin_square.coord
     add_to_origin = functools.partial(coord_add, origin)
     vectors = [(2, 1), (2, -1), (1, 2), (1, -2),
                (-2, 1), (-2, -1), (-1, 2), (-1, -2)]
@@ -28,3 +24,42 @@ def knight_moves(origin_square):
     heads = filter(is_valid_square_coord, map(add_to_origin, vectors))
 
     return heads
+
+
+def shortest_path(source, sink, moves):
+    """
+    Takes a source coordinate, sink coordinate, and a moves generator function
+    int tuple source: the starting coordinate
+    int tuple sink: the ending coordinate
+    (int tuple -> int tuple iterator) moves: a function that takes as input 
+        a coordinate and generates all reachable coordinates
+    returns (int, list of int tuples): returns length of shortest path as well
+        as the path itself in the form of a coordinate list
+    """
+    frontier = deque()
+    frontier.append(source)
+
+    # The dictionary maps coordinates to the previous node in the shortest path
+    # to that coordinate, as well as the length of the shortest path
+    prev_node_dict = {}
+    prev_node_dict[source] = (None, 0)
+
+    while(len(frontier) > 0):
+        node = frontier.popleft()
+        neighbor_dist = prev_node_dict[node][1] + 1
+        for neighbor in moves(node):
+            if neighbor not in prev_node_dict:
+                frontier.append(neighbor)
+                prev_node_dict[neighbor] = (node, neighbor_dist)
+                if neighbor == sink:
+                    frontier.clear()
+                    break
+
+    shortest_path = deque()
+    node = sink
+    while(node in prev_node_dict):
+        shortest_path.appendleft(node)
+        node = prev_node_dict[node][0]
+
+    return prev_node_dict[sink][1], shortest_path
+
--- chess_piece_test.py
+++ chess_piece_test.py
@@ -1,18 +1,16 @@
 import unittest
-from chess_square import (Square,
-                          name_to_coord,
-                          coord_to_name,
-                          is_valid_square_coord,
-                          is_valid_square_name)
-from chess_piece import knight_moves
+from chess_square import name_to_coord
+from chess_piece import (knight_moves,
+                         shortest_path)
+from collections import deque
 
 
 class TestChessPiece(unittest.TestCase):
 
     def setUp(self):
-        self.top_left_corner = Square.from_coord((0,0))
-        self.bot_right_corner = Square.from_coord((7,7))
-        self.e4 = Square.from_name('e4')
+        self.top_left_corner = (0,0)
+        self.bot_right_corner = (7,7)
+        self.e4 = name_to_coord('e4')
 
     def test_knight_moves(self):
         moves = list(knight_moves(self.top_left_corner))
@@ -22,6 +20,10 @@ class TestChessPiece(unittest.TestCase):
         moves = list(knight_moves(self.e4))
         self.assertEqual(moves, [(6, 5), (6, 3), (5, 6), (5, 2), (2, 5), (2, 3), (3, 6), (3, 2)])
 
+    def test_shortest_path(self):
+        self.assertEqual(shortest_path((0,0), (2,1), knight_moves), (1, deque([(0,0), (2,1)])))
+        self.assertEqual(shortest_path((0,0), (4,2), knight_moves), (2, deque([(0, 0), (2, 1), (4, 2)])))
+        self.assertEqual(shortest_path((0,0), (3,4), knight_moves), (3, deque([(0, 0), (2, 1), (4, 2), (3, 4)])))
 
 if __name__ == '__main__':
     unittest.main()
